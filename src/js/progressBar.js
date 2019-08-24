a729933fc1a27f2216be072d3c30bbef607a079f
zionHuang <hjszion@gmail.com>
2019-08-24 14:11:43 -0700

    finished progressBar Object-Oriented

--- src/js/progressBar.js
+++ src/js/progressBar.js
@@ -16,14 +16,16 @@ ProgressBar.prototype = {
         var innerRect = new Konva.Rect({
             x:this.x,
             y:this.y,
-            width:1/2*this.w,
+            width:0,
             height:this.h,
             fill:this.fillStyle, //只要设置了fill属性，那么konva就会填充这个矩形
             cornerRadius:(1/2)*this.h,
+            id:'innerRect',  //创建一个id 然后通过id选择器实现外部调用 相当于给dom元素添加了id属性
+            name:'innerRect',  //创建一个类  也可以通过.findOne('innerRect')来选择到这个元素
         });
 
         //定义的外部进度条的矩形
-        var outerRect = new Konva.Rect({
+        var outerRect = new Konva.Rect({ 
             x:this.x,
             y:this.y,
             width:this.w,
@@ -33,6 +35,7 @@ ProgressBar.prototype = {
             strokeWidth:4,
         });
 
+        //html:
         //打包成组
         this.group = new Konva.Group({
             x:0,
@@ -42,12 +45,23 @@ ProgressBar.prototype = {
         this.group.add(outerRect);
     },
     changeValue:function(val){
-        //传进来的进度
-        if(val >1){//1 - 100
+        //传进来的进度 到底到了%几
+        if(val >1){//1 - 100 vs 0-1  => 0.5
             val = val/100;
         }
+        //做动画 val = .3 .7 .9
+        var width = this.w * val;  //最终inner进度条的宽度
+        var innerRect = this.group.findOne('#innerRect');  //通过findOne来实现id选择器
+
+        //to动画系统:让我们的物件 变换到某个状态
+        //让物件从当前状态到下面设置的状态
+        innerRect.to({
+            width:width,
+            duration:0.3,
+            easing:Konva.Easings.EaseIn,
+        });
     },
-    addToGroupOrLayer:function(arg){
-        arg.add(this.group);
+    addToGroupOrLayer:function(layer){
+        layer.add(this.group);
     }
 }
